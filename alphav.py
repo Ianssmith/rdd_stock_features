7c20250b8ef402085c7b0740aa61c720e2153821
ianssmith <ian.s.smith0317@gmail.com>
2024-04-29 09:17:18 -0400

    alters program structure to run as python program. write to hdfs not working.

--- alphav.py
+++ alphav.py
@@ -26,26 +26,36 @@ for i in envdata:
 #csv
 #alvc = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey={env["alphav"]}&datatype=csv')
 #json
-dailyd = []
-for i in spdf['symbol'][2:3]:
-    print(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={i}&outputsize=full&apikey={env["alphav"]}')
-    alv_response = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={i}&outputsize=full&apikey={env["alphav"]}',verify=False)
-    #index 0 will be data index 1 is data symbol
-    dailyd.append([alv_response,i])
-    time.sleep(42)
+def get_from_alphav(start,stop):
+    dailyd = []
+    for i in spdf['symbol'][start:stop]:
+        print(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={i}&outputsize=full&apikey={env["alphav"]}')
+        alv_response = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={i}&outputsize=full&apikey={env["alphav"]}',verify=False)
+        #index 0 will be data index 1 is data symbol
+        dailyd.append([alv_response,i])
+        time.sleep(42)
+    dfs = []
+    for i in dailyd:
+        json_data = i[0].json()
+        print(json_data)
+        json_nometa = json_data['Time Series (Daily)']
+        df = pd.DataFrame.from_dict(json_nometa, orient='index')
+        ndf = df.reset_index()
+        fdf = ndf.rename(columns={"index":"date","1. open": "open", "2. high": "high","3. low":"low","4. close":"close","5. volume":"volume","delta":"delta"})
+        #again index 0 is data index 1 is data symbol
+        dfs.append([fdf,i[1]])
+        #write api calls to json file incase sparksession hdfs write does not work
+        with open(f'./data/{i[1]}.json', 'w') as outfile:
+            outfile.write(str(i[0].json()))
+    return dfs
+
+#call to get data from api
 dfs = []
-for i in dailyd:
-    json_data = i[0].json()
-    print(json_data)
-    json_nometa = json_data['Time Series (Daily)']
-    df = pd.DataFrame.from_dict(json_nometa, orient='index')
-    ndf = df.reset_index()
-    fdf = ndf.rename(columns={"index":"date","1. open": "open", "2. high": "high","3. low":"low","4. close":"close","5. volume":"volume","delta":"delta"})
-    #again index 0 is data index 1 is data symbol
-    dfs.append([fdf,i[1]])
-    #write api calls to json file incase sparksession hdfs write does not work
-    with open(f'./data/{i[1]}.json', 'w') as outfile:
-        outfile.write(str(i[0].json()))
+#dfs = get_from_alphav(3,4)
+pdf = pd.read_json('./data/ABT.json', lines=True)
+print(pdf)
+dfs.append(pdf)
+
 
 # Create SparkSession 
 spark = SparkSession.builder \
@@ -54,6 +64,10 @@ spark = SparkSession.builder \
       .getOrCreate() 
 
 for i in dfs:
+    #convert pandas dfs to pyspark dfs
     df_spark = spark.createDataFrame(i[0])
-    df_spark.write.save(f'/user/ec2-user/UKUSMarHDFS/ian/{i[1]}.parquet', format='parquet', mode='append')
+    df_spark.show()
+    #write to parquet file on hdfs
+    #df_spark.write.parquet(f'./data/{i[1]}.parquet',  mode='append')
+    #df_spark.write.parquet(f'/user/ec2-user/UKUSMarHDFS/ian/{i[1]}.parquet',  mode='append')
     #df_spark.write.save(f'/user/ec2-user/UKUSMarHDFS/ian/{i[1]}.parquet', format='parquet', mode='append')
