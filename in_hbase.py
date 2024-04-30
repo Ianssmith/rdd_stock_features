05401e1730a7c1f8f542f1562116cd782e173390
ianssmith <ian.s.smith0317@gmail.com>
2024-04-30 16:16:41 -0400

    adds spark(wip) and sql routines for creating external hive table from hdfs data. and screenshot

--- in_hbase.py
+++ in_hbase.py
@@ -4,49 +4,15 @@ from pyspark.sql.types import StructType
 
 spark = SparkSession.builder.master("local").appName("put_in_hbase").enableHiveSupport().getOrCreate()
 
-# Load JSON data from HDFS into DataFrame
-df = spark.read.json('hdfs://ip-172-31-3-80.eu-west-2.compute.internal:8020/user/ec2-user/UKUSMarHDFS/ian/proj/data/ACN.json')
-
-df.write.saveAsTable('ian_hive_external_alphav')
-
-df.createOrReplaceTempView("temp_json_table")
 
 # Define Hive table schema (optional: infer schema from DataFrame)
-hive_table_schema = "date STRING, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume INT"  # Modify according to your JSON schema
+hive_table_schema = "date date, open double, high double, low double, close double, volume double"  # Modify according to your JSON schema
 
 # Create external Hive table from temporary table
 spark.sql(f"CREATE EXTERNAL TABLE IF NOT EXISTS ian_hive_external_alphav \
            ({hive_table_schema}) \
-           ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'\
-           LOCATION 'hdfs://ip-172-31-3-80.eu-west-2.compute.internal:8020/user/ec2-user/UKUSMarHDFS/ian/proj/data/ACN.json'")
-
-'''
-stock_schema = StructType().add("date", "date")\
-	.add("open", "float")\
-	.add("high", "float")\
-	.add("low", "float")\
-	.add("close", "float")\
-	.add("volume", "integer")
-
-#df_spark.write.save(f'hdfs://ip-172-31-3-80.eu-west-2.compute.internal:8020/user/ec2-user/UKUSMarHDFS/ian/proj/data/{i[1]}.json', format='json', mode='append')
-df = spark.read.json('hdfs://ip-172-31-3-80.eu-west-2.compute.internal:8020/user/ec2-user/UKUSMarHDFS/ian/proj/data/ACN.json', schema=stock_schema)
-#df = spark.read.json("hdfs://localhost:9000/UKUSMarHDFS/ian/proj/data/ABT.json", schema=stock_schema)
-
-
-catalog = '{"table":{"namespace":"default","name":"ACN"},\
-	"rowkey":"key",\
-	"columns":{\
-	"col1":{"cf":"rowkey","col":"date","type":"date"},\
-	"col2":{"cf":"rowkey","col":"open","type":"float"},\
-	"col3":{"cf":"rowkey","col":"high","type":"float"},\
-	"col4":{"cf":"rowkey","col":"low","type":"float"},\
-	"col5":{"cf":"rowkey","col":"close","type":"float"},\
-	"col6":{"cf":"rowkey","col":"volume","type":int"}\
-}\
-}'
-
-df.write.options(catalog=catalog).format("org.apache.spark.sql.execution.datasources.hbase").save()
-
-notes
-
-'''
+			row format delimited\
+            fields terminated by ','\
+            stored as TEXTFILE\
+           lOCATION 'hdfs://ip-172-31-3-80.eu-west-2.compute.internal:8020/user/ec2-user/UKUSMarHDFS/ian/proj/data/'\
+           tblproperties(\"skip.header.line.count\"=\"1\")")
--- stock_hive_maker.sql
+++ stock_hive_maker.sql
@@ -1,12 +1,13 @@
 create external table if not exists alphav(
-    'date' date,
-    'open' float,
-    'high' float,
-    'low' float,
-    'close' float,
-    'volume' float
+    `date` date,
+    `open` double,
+    `high` double,
+    `low` double,
+    `close` double,
+    `volume` double
 )
 row format delimited
 fields terminated by ','
-stored as json
-location 'UKUSMarHDFS/ian/proj/data/ACN.json'
+stored as TEXTFILE
+LOCATION 'hdfs://ip-172-31-3-80.eu-west-2.compute.doubleernal:8020/user/ec2-user/UKUSMarHDFS/ian/proj/data/'
+tblproperties("skip.header.line.count"="1");
